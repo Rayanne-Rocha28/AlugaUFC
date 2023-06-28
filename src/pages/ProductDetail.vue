615024d6a9d724a0aaa48763e9e5f343fbe08963
Rayanne Rocha <rayannerocha.rocha@gmail.com>
2023-06-27 23:47:20 -0300

    correção pdp

--- src/pages/ProductDetail.vue
+++ src/pages/ProductDetail.vue
@@ -6,27 +6,31 @@
         ltSm ? 'q-px-sm q-py-md' : 'container-padding',
       ]"
     >
-      <router-link to="/" class="text-center">
-        <q-img
-          class="self-center"
-          src="~assets/logo-horizontal.svg"
-          width="250px"
-          height="100px"
+      <div class="row items-center">
+        <span class="text-h6">Detalhes do anúncio</span>
+        <q-space /><q-btn
+          color="primary"
+          push
+          icon="arrow_back_ios"
+          label="Voltar"
+          @click="$router.back()"
+          no-caps
         />
-      </router-link>
+      </div>
 
       <q-form
-        @submit="onSubmit"
+        @submit="() => {}"
         @reset="() => {}"
         class="row justify-center q-my-md q-col-gutter-sm"
       >
         <q-input
+          readonly
           dense
           class="col-md-12 col-sm-12 col-xs-12"
           name="adTitle"
           filled
           v-model="adV2.description"
-          label="Titulo do anúncio *"
+          label="Titulo do anúncio"
           lazy-rules
           :rules="[
             (val) =>
@@ -39,12 +43,69 @@
         </q-input>
 
         <q-input
+          readonly
+          dense
+          class="col-md-12 col-sm-12 col-xs-12"
+          name="adMail"
+          filled
+          v-model="adV2.mail"
+          label="Email de contato"
+          lazy-rules
+          :rules="[
+            (val) => (val && val.length > 3) || 'Por favor insira o email',
+          ]"
+        >
+          <template v-slot:prepend>
+            <q-icon name="mail" />
+          </template>
+        </q-input>
+
+        <q-input
+          readonly
+          dense
+          class="col-md-12 col-sm-12 col-xs-12"
+          name="adPhone"
+          filled
+          v-model="adV2.phone"
+          label="Telefone de contato"
+          mask="(##) #####-####"
+          lazy-rules
+          :rules="[
+            (val) => (val && val.length > 13) || 'Por favor insira o telefone',
+          ]"
+        >
+          <template v-slot:prepend>
+            <q-icon name="phone_iphone" />
+          </template>
+        </q-input>
+
+        <q-input
+          readonly
+          dense
+          class="col-md-12 col-sm-12 col-xs-12"
+          name="adValue"
+          filled
+          v-model="adV2.value"
+          label="Valor"
+          prefix="R$"
+          lazy-rules
+          mask="#.##"
+          reverse-fill-mask
+          :rules="[(val) => (val && val > 0) || 'Por favor insira o valor']"
+        >
+          <template v-slot:prepend>
+            <q-icon name="money" />
+          </template>
+        </q-input>
+
+        <q-input
+          readonly
           dense
           class="col-md-12 col-sm-12 col-xs-12"
           name="adAddressStreet"
           filled
           v-model="adV2.addressStreet"
-          label="Rua *"
+          label="Rua"
           lazy-rules
           :rules="[
             (val) => (val && val.length > 3) || 'Por favor insira a rua',
@@ -56,12 +117,13 @@
         </q-input>
 
         <q-input
+          readonly
           dense
           class="col-md-4 col-sm-4 col-xs-12"
           name="adAddressNumber"
           filled
           v-model="adV2.addressNumber"
-          label="Número *"
+          label="Número"
           lazy-rules
           :rules="[
             (val) =>
@@ -74,12 +136,13 @@
         </q-input>
 
         <q-input
+          readonly
           dense
           class="col-md-8 col-sm-8 col-xs-12"
           name="adAddressDistrict"
           filled
           v-model="adV2.addressDistrict"
-          label="Bairro *"
+          label="Bairro"
           lazy-rules
           :rules="[
             (val) => (val && val.length > 3) || 'Por favor insira o bairro',
@@ -91,13 +154,14 @@
         </q-input>
 
         <q-input
+          readonly
           dense
-          class="col-md-6 col-sm-12 col-xs-12"
+          class="col-6"
           name="adBedrooms"
           filled
           type="number"
           v-model="adV2.bedrooms"
-          label="Quartos *"
+          label="Quartos"
           lazy-rules
           mask="##"
           :rules="[
@@ -111,13 +175,14 @@
         </q-input>
 
         <q-input
+          readonly
           dense
-          class="col-md-6 col-sm-12 col-xs-12"
+          class="col-6"
           name="adBathroom"
           filled
           type="number"
           v-model="adV2.bathrooms"
-          label="Banheiros *"
+          label="Banheiros"
           mask="##"
           lazy-rules
           :rules="[
@@ -141,6 +206,7 @@
             :dense="ltSm"
             toggle-color="primary"
             text-color="primary"
+            readonly
             :options="[
               {
                 value: 'ONLY_MAN',
@@ -198,6 +264,7 @@
             :dense="ltSm"
             toggle-color="primary"
             text-color="primary"
+            readonly
             :options="[
               {
                 label: 'Apenas Compartilhar',
@@ -211,82 +278,25 @@
           />
         </div>
 
-        <q-input
-          dense
-          class="col-md-12 col-sm-12 col-xs-12"
-          name="adValue"
-          filled
-          v-model="adV2.value"
-          label="Valor *"
-          prefix="R$"
-          lazy-rules
-          mask="#.##"
-          reverse-fill-mask
-          :rules="[(val) => (val && val > 0) || 'Por favor insira o valor']"
-        >
-          <template v-slot:prepend>
-            <q-icon name="money" />
-          </template>
-        </q-input>
+        <div class="col-md-12 col-sm-12 col-xs-12 q-pb-sm">
+          <q-btn
+            class="full-width"
+            color="primary"
+            icon="visibility"
+            label="Ver Fotos"
+            @click="dialog = true"
+            no-caps
+          />
+        </div>
 
         <q-input
-          dense
-          class="col-md-12 col-sm-12 col-xs-12"
-          name="adMail"
-          filled
-          v-model="adV2.mail"
-          label="Email de contato *"
-          lazy-rules
-          :rules="[
-            (val) => (val && val.length > 3) || 'Por favor insira o email',
-          ]"
-        >
-          <template v-slot:prepend>
-            <q-icon name="mail" />
-          </template>
-        </q-input>
-
-        <q-input
-          dense
-          class="col-md-12 col-sm-12 col-xs-12"
-          name="adPhone"
-          filled
-          v-model="adV2.phone"
-          label="Telefone de contato *"
-          mask="(##) #####-####"
-          lazy-rules
-          :rules="[
-            (val) => (val && val.length > 13) || 'Por favor insira o telefone',
-          ]"
-        >
-          <template v-slot:prepend>
-            <q-icon name="phone_iphone" />
-          </template>
-        </q-input>
-
-        <q-file
-          multiple
-          dense
-          filled
-          class="col-md-12 col-sm-12 col-xs-12"
-          use-chips
-          v-model="adV2.cover"
-          accept=".jpg, .jpeg, .png"
-          @rejected="onRejected"
-          :rules="[(val) => true]"
-          label="Fotos"
-        >
-          <template v-slot:prepend>
-            <q-icon name="image" />
-          </template>
-        </q-file>
-        <q-input
+          readonly
           dense
           class="col-md-12 col-sm-12 col-xs-12"
           name="aluDescription"
           filled
           v-model="adV2.brief"
-          label="Descrição *"
+          label="Descrição"
           lazy-rules
           type="textarea"
           :rules="[(val) => (val) => true]"
@@ -295,19 +305,65 @@
             <q-icon name="description" />
           </template>
         </q-input>
-
-        <div class="col-12 text-center q-gutter-y-sm">
-          <q-btn
-            class="full-width"
-            type="submit"
-            color="primary"
-            label="Cadastrar anúncio"
-            size="md"
-            :loading="loading"
-            no-caps
-          />
-        </div>
       </q-form>
+
+      <q-dialog
+        v-model="dialog"
+        transition-show="slide-up"
+        transition-hide="slide-down"
+      >
+        <q-card style="min-height: 260px; min-width: 360px; width: 90%">
+          <q-card-section class="q-pa-none">
+            <div class="row">
+              <q-space />
+              <q-btn
+                color="dark"
+                round
+                flat
+                icon="close"
+                @click="dialog = false"
+              />
+            </div>
+          </q-card-section>
+
+          <q-card-section class="q-pt-none">
+            <q-carousel
+              animated
+              v-model="adV2.slide"
+              :arrows="!!adV2.photos && adV2.photos.length > 1"
+              infinite
+              :thumbnails="!!adV2.photos && adV2.photos.length > 1"
+              :fullscreen="fullscreenCarousel"
+            >
+              <q-carousel-slide
+                v-for="(photo, index) in adV2.photos"
+                :key="photo + index"
+                :name="index"
+                :img-src="photo"
+              />
+              <q-carousel-slide
+                v-if="!adV2.photos || adV2.photos.length == 0"
+                :name="0"
+                img-src="~assets/2932347_home_house_building_estate_icon.svg"
+              />
+              <template v-slot:control>
+                <q-carousel-control position="top-right" :offset="[18, 18]">
+                  <q-btn
+                    round
+                    dense
+                    color="white"
+                    text-color="primary"
+                    :icon="
+                      fullscreenCarousel ? 'fullscreen_exit' : 'fullscreen'
+                    "
+                    @click="fullscreenCarousel = !fullscreenCarousel"
+                  />
+                </q-carousel-control>
+              </template>
+            </q-carousel>
+          </q-card-section>
+        </q-card>
+      </q-dialog>
     </div>
   </q-page>
 </template>
@@ -315,102 +371,52 @@
 import { useQuasar } from "quasar";
 import { ref, computed } from "vue";
 import {
-  getStorage,
-  uploadBytes,
-  ref as storageRef,
-  getDownloadURL,
-} from "firebase/storage";
-import { addDoc, getDoc, collection, doc } from "firebase/firestore";
-import { app, db } from "app/firebaseConfig";
+  getFirestore,
+  collection,
+  query,
+  where,
+  getDocs,
+} from "firebase/firestore";
+import { app } from "app/firebaseConfig";
 import { getAuthUser, isAuthenticated } from "../utils/authentication";
 export default {
-  name: "ProductFormPage",
+  name: "ProductDetailPage",
   setup() {
     const $q = useQuasar();
     const ltSm = computed(() => {
       return $q.screen.lt.sm;
     });
 
-    const storage = getStorage(app);
+    const adV2 = ref({});
 
-    const adV2 = ref({
-      description: "",
-      addressStreet: "",
-      addressNumber: "",
-      addressDistrict: "",
-      addressCity: "Russas",
-      addressState: "CE",
-      googleMapsURL: "",
-      brief: "",
-      bedrooms: 1,
-      bathrooms: 1,
-      sexType: "BOTH",
-      type: "SHARE_ROOM",
-      phone: "",
-      mail: "",
-      value: "",
-      status: "OPEN",
-      photos: [],
-      cover: "",
-      googleMapsURL: "",
-      userId: "",
-      clicksCount: 0,
-    });
-
-    return { adV2, storage, getAuthUser, isAuthenticated, ltSm };
+    return {
+      adV2,
+      getAuthUser,
+      isAuthenticated,
+      ltSm,
+      dialog: ref(false),
+      maximizedToggle: ref(true),
+      fullscreenCarousel: ref(false),
+    };
   },
   methods: {
-    onSubmit() {
-      const notif = this.$q.notify({
-        type: "ongoing",
-        message: "Registrando anúncio, aguarde...",
-        timeout: 3000,
-      });
-      try {
-        this.uploadImage(this.adV2.cover).then(async (img) => {
-          const docRef = await addDoc(collection(db, "adsV2"), {
-            ...this.adV2,
-            photos: img,
-            cover: img[0] || "",
-            userId: getAuthUser().id,
-          });
-          notif({
-            type: "positive",
-            message: "Anúncio registrado com sucesso!",
-            timeout: 1000,
-          });
-          this.$router.push("/products");
+    async getProduct(id) {
+      const db = getFirestore(app);
+      const adsCollection = collection(db, "adsV2");
+      const data = [];
+      const myQuery = query(adsCollection, where("id", "==", id));
+
+      const querySnapshot = await getDocs(myQuery);
+      if (!querySnapshot.empty)
+        querySnapshot.forEach((item) => {
+          this.adV2 = { ...item.data(), id: item.id, slide: 0 };
         });
-      } catch (error) {
-        notif({
-          type: "negative",
-          message: "Ocorreu um erro durante o cadastro do anuncio!",
-          timeout: 1000,
-        });
-      }
-    },
-    async uploadImage(event) {
-      const images = [];
-      if (!!event && event.length > 0)
-        for (let img of event) {
-          try {
-            const file = img;
-            const ref = storageRef(this.storage, `images/${file.name}`);
-            const snapshot = await uploadBytes(ref, file);
-            const uri = await getDownloadURL(snapshot.ref);
-            images.push(uri);
-          } catch (error) {
-            console.error("Erro ao enviar imagem:", error);
-          }
-        }
-      return images;
     },
   },
   mounted() {
-    if (this.isAuthenticated()) {
-      this.user = this.getAuthUser();
-      this.adV2.mail = this.user.email;
-    } else this.$router.push("/login");
+    if (!!this.$route.params.id) {
+      this.getProduct(this.$route.params.id);
+    } else this.$router.push("/not-found");
   },
 };
 </script>
